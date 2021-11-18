49232cb2673bfbc29c962f677aaf541b5984f02e
TerryMXJ <terrymeng0820@163.com>
2021-11-18 21:05:33 +0800

    [update] sliding window

---
+++ array/SlidingWindow.java
@@ -0,0 +1,24 @@
+package array;
+
+public class SlidingWindow {
+
+    public static void main(String[] args) {
+
+    }
+
+    private int slidingWindow(int[] nums, int target) {
+        // 创建头指针, 状态变量信息
+        int i = 0, sum = 0, res = 0;
+        // 移动尾指针
+        for (int j = 0; j < nums.length; j++) {
+            // 更新状态信息
+            sum += nums[j];
+            // 移动尾指针
+            while (sum > target) {
+                // 判断情况, 情况合适更新res
+            }
+        }
+        return res;
+    }
+
+}
